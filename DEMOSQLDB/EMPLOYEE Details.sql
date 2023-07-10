2083237a7cbbf0944a3b8873e3934caefd9039f1
US\pragysahoo <pragysahoo@deloitte.com>
2023-07-10 17:01:49 +0530

    Created Data

---
+++ DEMOSQLDB/EMPLOYEE Details.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[EMPLOYEE Details] (
+    [Name]       CHAR (10) NOT NULL PRIMARY KEY CLUSTERED ([Name] ASC),
+    [Place]      CHAR (10) NULL,
+    [Department] CHAR (10) NULL,
+    [ID]         INT       NULL
+);
